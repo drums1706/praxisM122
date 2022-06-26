7a716b27b16b7f4ee81d5191d2eedf443f1c77ca
easyboy1706 <62602436+easyboy1706@users.noreply.github.com>
2022-06-26 18:13:34 +0200

    Update CreateUser.sh

--- praxisarbeit/bin/CreateUser.sh
+++ praxisarbeit/bin/CreateUser.sh
@@ -6,7 +6,6 @@ BINDIR=`pwd`
 cd $cwd
 BASENAME=`basename $0`
 ETCDIR=$BINDIR/../etc
-#. $ETCDIR/$BASENAME # Load config file
 
 usage() {
     echo "Usage: $BASENAME [users file] [options]"
@@ -23,26 +22,26 @@ done
 
 USERS_FILE=$1
 if [ ! -f "$USERS_FILE" ]; then
-    echo "Error: users file ($USERS_FILE) doesnt exist."
+    echo "ERROR: users file ($USERS_FILE) doesnt exist."
     exit 3
 fi
 
 cat "$USERS_FILE" | while read username groupname firstname lastname; do
     if [ ! "$(getent group $groupname)" ]; then # check if group exists
-        echo "Warning: group $groupname doesn't exists. Create new group..."
+        echo "WARNING: group $groupname doesn't exists. Create new group..."
         groupadd $groupname
     fi
 
     if [ ! "$(grep ^$groupname$ $ETCDIR/groups.conf)" ]; then # check if group is backed-up by script 2
-        echo "Warning: group $groupname is not a backed-up group."
+        echo "WARNING: group $groupname is not a backed-up group."
     fi
    
     if [ "$(getent passwd $username)" ]; then # check if user already exists
-        echo "Warning: user $username already exists. Skipping..."
+        echo "WARNING: user $username already exists. Skipping..."
         continue
     fi
 
     useradd "$username" -g "$groupname" -m -k /etc/skel -p $(openssl passwd -1 "$PASSWORD") -c "$firstname $lastname"
     passwd --expire "$username" > /dev/null
-    echo "Created user $username ($firstname $lastname) with group $groupname."
+    echo "SUCCESS: Created user $username ($firstname $lastname) with group $groupname."
 done
